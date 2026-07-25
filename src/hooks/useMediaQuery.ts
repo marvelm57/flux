d2ea28eb7ab59c3484eddc92eda87e5daa26ffff
marvelm57 <vivalamarvel@gmail.com>
2026-07-26 00:30:39 +0700

    fix: change mobile view breakpoint

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -21,5 +21,5 @@ export function useMediaQuery(query: string): boolean {
 }
 
 export function useIsMobile(): boolean {
-  return useMediaQuery('(max-width: 768px)');
+  return useMediaQuery('(max-width: 1023px)');
 }
